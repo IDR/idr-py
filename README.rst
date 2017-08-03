92f084c0e48b412131d4c671268ba77d4f23aac4
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-08-03 15:06:28 +0100

    Add file required for pip installation

---
+++ README.rst
@@ -0,0 +1,38 @@
+.. image:: https://travis-ci.org/ome/omero-fpbioimage.svg?branch=master
+    :target: https://travis-ci.org/ome/omero-fpbioimage
+
+.. image:: https://badge.fury.io/py/omero-fpbioimage.svg
+    :target: https://badge.fury.io/py/omero-fpbioimage
+
+IDR-PY
+======
+
+TO BE ADDED
+
+Requirements
+============
+
+TO BE ADDED
+
+Installing from PyPI
+====================
+
+This section assumes that an OMERO.web is already installed.
+
+
+Install the app using `pip <https://pip.pypa.io/en/stable/>`_:
+
+::
+
+    $ pip install -U idr-py
+
+
+License
+-------
+
+TO BE ADDED 
+
+Copyright
+---------
+
+2017, The Open Microscopy Environment
---
+++ setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#
+# Copyright (c) 2017 University of Dundee.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# FPBioimage was originally published in
+# <https://www.nature.com/nphoton/journal/v11/n2/full/nphoton.2016.273.html>.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Version: 1.0
+#
+import os
+from setuptools import setup, find_packages
+
+
+version = "0.0.1"
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name="idr-py",
+      packages=find_packages(exclude=['ez_setup']),
+      version=version,
+      description="A collection of python files to analyze data",
+      long_description=read('README.rst'),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Science/Research',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+          'Topic :: Internet :: WWW/HTTP :: WSGI',
+          'Topic :: Software Development :: Libraries :: '
+          'Application Frameworks',
+          'Topic :: Text Processing :: Markup :: HTML'
+      ],  # Get strings from
+          # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      author='The Open Microscopy Team',
+      author_email='ome-devel@lists.openmicroscopy.org.uk',
+      license='AGPL-3.0',
+      url="https://github.com/IDR/idr-py",
+      download_url='https://github.com/IDR/idr-py/archive/v%s.tar.gz' % version,  # NOQA
+      keywords=['Python', 'plugin'],
+      include_package_data=True,
+      zip_safe=False,
+      )
