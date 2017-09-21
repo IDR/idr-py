ff8699780474eb5e774005637ca46a52ce4d7e06
bramalingam <b.ramalingam@dundee.ac.uk>
2017-09-21 17:11:28 +0100

    Flake8 cheat

--- idr/attributes.py
+++ idr/attributes.py
@@ -2,7 +2,7 @@
 Helper functions for accessing the IDR from within IPython notebooks.
 """
 import pandas
-import omero.clients
+import omero.clients # NOQA
 from omero.rtypes import rlist, rstring, unwrap
 from omero.sys import ParametersI
 from externalDBs import get_entrezid, get_ensembleid, ensembleid_to_genesymbol
