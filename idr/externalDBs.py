c82b6977b347c406ab0725d61bcdc14091dadce5
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-12-17 21:41:37 +0000

    roll back change, it does not work

--- idr/externalDBs.py
+++ idr/externalDBs.py
@@ -76,7 +76,7 @@ def get_ensembleid(gene):
     ensembleserver = "https://rest.ensembl.org/xrefs/symbol/homo_sapiens/"
     url = ensembleserver + gene + "?content-type=application/json"
 
-    res = requests.get(url, verify=False)
+    res = requests.get(url)
     results = pandas.read_json(StringIO(res.text))
 
     ensembleid = []
@@ -106,7 +106,7 @@ def ensembleid_to_genesymbol(ensembleid):
         + ensembleid
         + "?content-type=application/json;external_db=WikiGene"
     )
-    res = requests.get(url, verify=False)
+    res = requests.get(url)
     if not res.ok:
         return ensembleid
     if "error" in res.text:
